9976f889aa9e9b386603c1862f1c4818ba9eff27
kattana <kattanaluz@gmail.com>
2022-01-19 09:18:09 +0000

    toggleButton function is working

--- task-tracker/src/Components/App/App.js
+++ task-tracker/src/Components/App/App.js
@@ -1,35 +1,36 @@
 import "./App.css";
 import DisplayList from "../List";
 import Button from "../Button";
-import {useState} from 'react';
+import { useState } from "react";
 
 function App() {
-  /* our initial state should be the value of isCompleted property in our workshops array
-  we need to pull the isCompleted property 
-  [...workshops,]
-  */
-
   const workshops = [
     { title: "Hackathon", date: "10/12/2021", isCompleted: false },
     { title: "Hackathon", date: "10/12/2021", isCompleted: false },
     { title: "Hackathon", date: "10/12/2021", isCompleted: false },
-    { title: "Hackathon", date: "10/12/2021", isCompleted: false }
+    { title: "Hackathon", date: "10/12/2021", isCompleted: false },
   ];
 
   const [toggleIsCompleted, setToggleIsCompleted] = useState(workshops);
 
-  function toggleButton(index){
+  function toggleButton(index) {
     console.log(`I am toggling ${index} in ${workshops}`);
-    const newWorkshop = [...workshops.slice(0,index), {...workshops[index], isCompleted: !(workshops[index].isCompleted)}, ...workshops(index + 1)];
+    const newWorkshop = [
+      ...toggleIsCompleted.slice(0, index),
+      {
+        ...toggleIsCompleted[index],
+        isCompleted: !toggleIsCompleted[index].isCompleted,
+      },
+      ...toggleIsCompleted.slice(index + 1),
+    ];
     setToggleIsCompleted(newWorkshop);
     console.log(newWorkshop);
   }
 
-
   return (
     <div className="App">
       <DisplayList workshops={workshops} onClick={toggleButton} />
-      <Button title='Return'/>
+      <Button title="Return" />
     </div>
   );
 }
